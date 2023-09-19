5b96c8e929756c1d3c0d3a233bb3a09980cf296d
SaiSanthosh12 <132330867+SaiSanthosh12@users.noreply.github.com>
2023-09-19 23:58:38 +0530

    Create sizedatatypes.java

---
+++ sizedatatypes.java
@@ -0,0 +1,15 @@
+class Sizedatatypes {
+    public static void main(String[] args) {
+        
+        System.out.println("Size of Byte:"+(Byte.SIZE/8));
+        System.out.println("Size of Integer:"+(Integer.SIZE/8));
+        System.out.println("Size of Short:"+(Short.SIZE/8));
+        System.out.println("Size of Long:"+(Long.SIZE/8));
+        System.out.println("Size of Float:"+(Float.SIZE/8));
+        System.out.println("Size of Double:"+(Double.SIZE/8));
+        System.out.println("Size of Character:"+(Character.SIZE/8));
+        
+        //System.out.println("Size of String:"+(String.SIZE/8));
+        //System.out.println("Size of Boolean:"+(Boolean.SIZE/8));
+    }
+}
