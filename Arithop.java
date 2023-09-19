22b5a6a856f3223fd19fc37db8e10bb84177e2c1
SaiSanthosh12 <132330867+SaiSanthosh12@users.noreply.github.com>
2023-09-20 00:14:15 +0530

    Create Arithop.java

---
+++ Arithop.java
@@ -0,0 +1,19 @@
+public class Arithop {
+    
+    public static void main(String[] args)
+    { 
+        int a =10,b =5;
+        System.out.println("+:"+(a+b)); //Addition
+        System.out.println("-:"+(a-b));//Subtraction
+        System.out.println("*:"+(a*b));// Multiplication
+        System.out.println("/:"+(a/b));//Division
+        System.out.println("%:"+(a%b));//Modulus
+    }
+}
+
+output:
++:15
+-:5
+*:50
+/:2
+%:0
