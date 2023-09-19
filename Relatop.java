b706f0b16d18bc73eb886827daf3ce369d4a2a2b
SaiSanthosh12 <132330867+SaiSanthosh12@users.noreply.github.com>
2023-09-20 00:34:24 +0530

    Create Relatop.java

---
+++ Relatop.java
@@ -0,0 +1,21 @@
+public class Relatop{
+    
+    public static void main(String[] args)
+    { 
+        int a =10,b =5;
+        System.out.println("==:"+(a==b));  //Equal to operator
+        System.out.println("!=:"+(a!=b));  //Not Equal to Op
+        System.out.println("> :"+(a>b));   //Greater Than Op
+        System.out.println(">=:"+(a>=b));  // Greater Than or equals to Op 
+        System.out.println("< :"+(a<b));   //Less than Op
+        System.out.println("<=:"+(a<=b));  //Less Than or Equals to Op
+    }
+}
+
+output:
+==:false
+!=:true
+> :true
+>=:true
+< :false
+<=:false
